e3085497a78b8cc08990dc7b743aa30c0c2ec9cc
IDCjaja <838927992@qq.com>
2018-06-26 19:17:12 +0800

    Replace Helloworld to admin

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import admin from '@/components/admin'
 
-describe('HelloWorld.vue', () => {
+describe('admin.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(admin)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .toEqual('Welcome to Your Vue.js App')
