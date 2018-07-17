b1641f3a90590d700d03171745909c1a815e317a
limengjia <http://en.mail.qq.com/cgi-bin/frame_html?sid=ybETBwFeFBUnOQtR&r=3baf7e1446eff88e726ff3579b3a65fb>
2018-07-17 15:49:25 +0800

    Modify filter icon color

--- src/components/admin.vue
+++ src/components/admin.vue
@@ -45,7 +45,7 @@
               <svg height="16px" width="16px">
                 <use :xlink:href="'#chooseIcon'+item.categoryId" :fill="item.color" :stroke="item.color"></use>
               </svg>
-            {{item.title}}</span>
+            {{item.name}}</span>
             <div class="window-edit info">
               <tag-edit-window
                 @toggleInitMap="initMap"
@@ -128,8 +128,8 @@ export default {
       searchResult: false,
       markerClustererShow: false,
       currentMarkerId: -1,
-      currentMarkerTagId: 1,
-      currentMarkerCategoryId: 1,
+      currentMarkerTagId: -1,
+      currentMarkerCategoryId: -1,
       markerClusterList: [],
       activeName: "0",
       events: {
@@ -149,183 +149,6 @@ export default {
         position: [0,0],
         visible: false
       },
-      markerList: [
-        {
-          id: 1,
-          longitude: 104.106946,
-          latitude: 30.674249,
-          title: '妈妈驿站',
-          tagId: 1,
-          categoryId:1
-        },
-        {
-          id: 11,
-          longitude: 104.106946,
-          latitude: 30.674253,
-          title: '华贸广场',
-          tagId: 1,
-          categoryId:1
-        },
-        {
-          id: 2,
-          longitude: 104.109191,
-          latitude: 30.671637,
-          title: '伊藤洋华堂(建设路店)',
-          tagId: 1,
-          categoryId: 1,
-          address: '二环路东二段426号阳光新生活广场'
-        },
-        {
-          id: 3,
-          longitude: 114.396345,
-          latitude: 30.9455,
-          title: '成都',
-          tagId: 1,
-          categoryId: 1
-        },
-        {
-          id: 4,
-          longitude: 104.101876,
-          latitude: 30.668877,
-          title: '第五大道',
-          tagId: 1,
-          categoryId: 1,
-          address: '建设路26号'
-        },
-        {
-          id: 5,
-          longitude: 121.5273285,
-          latitude: 31.21515044,
-          title: '上海',
-          tagId: 1,
-          categoryId: 1
-        },
-        {
-          id: 6,
-          longitude: 104.108444,
-          latitude: 30.670595,
-          title: '建设路东站(公交站)',
-          tagId: 1,
-          categoryId: 1,
-          address: '6路;14路;42路;71路;72路;社区巴士1010路'
-        },
-        {
-          id: 7,
-          longitude: 104.104973,
-          latitude: 30.670078,
-          title: '万科·钻石广场',
-          tagId: 1,
-          categoryId: 1,
-          address: '建设路10号'
-        },
-        {
-          id: 8,
-          longitude: 104.104147,
-          latitude: 30.669054,
-          title: '高地中心',
-          tagId: 1,
-          categoryId: 1,
-          address: '建设路9号'
-        },
-        {
-          id: 9,
-          longitude: 104.075275,
-          latitude: 30.601965,
-          title: '成都高新技术产业开发区桂溪街道办事处',
-          tagId: 1,
-          categoryId: 1,
-          address: '天仁路176号'
-        },
-        {
-          id: 10,
-          longitude: 104.077467,
-          latitude: 30.618422,
-          title: '桂溪加油站(科华中路)',
-          tagId: 1,
-          categoryId: 1,
-          address: '科华中路63号'
-        },
-        {
-          id: 12,
-          longitude: 104.062087,
-          latitude: 30.599805,
-          title: '宜家家居(成都商场店)',
-          tagId: 1,
-          categoryId: 1,
-          address: '四川省成都市武侯区桂溪街道宜家家居(成都商场店)'
-        },
-        {
-          id: 13,
-          longitude: 104.06183,
-          latitude: 30.601754,
-          title: '欧尚(成都高新店)',
-          tagId: 1,
-          categoryId: 1,
-          address: '四川省成都市武侯区桂溪街道欧尚(成都高新店)'
-        },
-        {
-          id: 14,
-          longitude: 104.063986,
-          latitude: 30.599547,
-          title: '迪卡侬',
-          tagId: 1,
-          categoryId: 1,
-          address: '四川省成都市武侯区桂溪街道都会路199号迪卡侬运动场'
-        },
-        {
-          id: 15,
-          longitude: 104.060961,
-          latitude: 30.601255,
-          title: '必胜客',
-          tagId: 1,
-          categoryId: 1,
-          address: '四川省成都市武侯区桂溪街道必胜客(成都高新店)'
-        },
-        {
-          id: 16,
-          longitude: 104.060102,
-          latitude: 30.601394,
-          title: '宜家家居(成都商场店)',
-          tagId: 1,
-          categoryId: 1,
-          address: '四川省成都市武侯区石羊场街道益州大道北段360号英祥·财富领域'
-        },
-        {
-          id: 17,
-          longitude: 104.062903,
-          latitude: 30.601855,
-          title: '凯德广场',
-          tagId: 1,
-          categoryId: 1,
-          address: '四川省成都市武侯区桂溪街道南洋小馆(凯德广场)凯德广场(新南店)'
-        },
-        {
-          id: 18,
-          longitude: 104.062087,
-          latitude: 30.599805,
-          title: '宜家家居(成都商场店)',
-          tagId: 1,
-          categoryId: 1,
-          address: '四川省成都市武侯区桂溪街道宜家家居(成都商场店)'
-        },{
-          id: 19,
-          longitude: 104.05815,
-          latitude: 30.599565,
-          title: '城市春天',
-          tagId: 1,
-          categoryId: 1,
-          address: '四川省成都市武侯区石羊场街道成都云起茶宿客栈城市春天'
-        },
-        {
-          id: 20,
-          longitude: 104.062087,
-          latitude: 30.599805,
-          title: '四川省城市车辆置业有限责任公司',
-          tagId: 1,
-          categoryId: 1,
-          address: '四川省成都市武侯区石羊场街道火车南站西路865号四川省城市车辆置业有限责任公司'
-        }
-      ],
       tagIcon: '',
       tagColor: '',
       categories: [
@@ -377,27 +200,48 @@ export default {
         {id:4,name: '待整改', color: '#feb902', number: '0',defaultColor: false}
       ],
       searchResultList: [
-        {id:1,name: '企业', address:'dhfkjsdfsdfklsdjf.',longitude: 104.106946,latitude: 30.674249,},
-        {id:2,name: '个人', address:'dhfkjsdfsdfklsdvbxcvkjjdlhlvsdkljf.',longitude: 104.109191,latitude: 30.671637,},
-        {id:3,name: '法人', address:'dhfkjsdfsdfklsdjf.',longitude: 114.396345,latitude: 30.9454,},
-        {id:4,name: '企业', address:'dhfkjsdfsdfklsdjf.',longitude: 104.101876,latitude: 30.668877,},
-        {id:5,name: '个人', address:'dhfkjsdfsdfklsdvbxcvkjjdlhlvsdkljf.',longitude: 121.5273285,latitude: 31.21515044,},
-        {id:6,name: '法人', address:'dhfkjsdfsdfklsdjf.',longitude: 104.108444,latitude: 30.670595,},
-        {id:7,name: '企业', address:'dhfkjsdfsdfklsdjf.',longitude: 104.104973,latitude: 30.670078,},
-        {id:8,name: '个人', address:'dhfkjsdfsdfklsdvbxcvkjjdlhlvsdkljf.',longitude: 104.077467,latitude: 30.618422,},
-        {id:9,name: '法人', address:'dhfkjsdfsdfklsdjf.'},
-        {id:10,name: '企业', address:'dhfkjsdfsdfklsdjf.'},
-        {id:11,name: '个人', address:'dhfkjsdfsdfklsdvbxcvkjjdlhlvsdkljf.',longitude: 104.106946, latitude: 30.674253,},
-        {id:12,name: '法人', address:'dhfkjsdfsdfklsdjf.'},
-        {id:1,name: '企业', address:'dhfkjsdfsdfklsdjf.'},
-        {id:2,name: '个人', address:'dhfkjsdfsdfklsdvbxcvkjjdlhlvsdkljf.'},
-        {id:3,name: '法人', address:'dhfkjsdfsdfklsdjf.'},
-        {id:1,name: '企业', address:'dhfkjsdfsdfklsdjf.'},
-        {id:2,name: '个人', address:'dhfkjsdfsdfklsdvbxcvkjjdlhlvsdkljf.'},
-        {id:3,name: '法人', address:'dhfkjsdfsdfklsdjf.'},
-        {id:1,name: '企业', address:'dhfkjsdfsdfklsdjf.'},
-        {id:2,name: '个人', address:'dhfkjsdfsdfklsdvbxcvkjjdlhlvsdkljf.'},
-        {id:3,name: '法人', address:'dhfkjsdfsdfklsdjf.'}
+        {id: 1,longitude: 104.106946,latitude: 30.674249,name: '妈妈驿站',tagId: 1,categoryId:1},
+        {id: 11,longitude: 104.106946,latitude: 30.674253,name: '华贸广场',tagId: 1,categoryId:1},
+        {id: 2,longitude: 104.109191,latitude: 30.671637,name: '伊藤洋华堂(建设路店)',tagId: 1,categoryId: 1,address: '二环路东二段426号阳光新生活广场'},
+        {id: 3,longitude: 114.396345,latitude: 30.9455,name: '成都',tagId: 1,categoryId: 1},
+        {id: 4,longitude: 104.101876,latitude: 30.668877,name: '第五大道',tagId: 1,categoryId: 1,address: '建设路26号'},
+        {id: 5,longitude: 121.5273285,latitude: 31.21515044,name: '上海',tagId: 1,categoryId: 1},
+        {id: 6,longitude: 104.108444,latitude: 30.670595,name: '建设路东站(公交站)',tagId: 1,categoryId: 1,address: '6路;14路;42路;71路;72路;社区巴士1010路'},
+        {id: 7,longitude: 104.104973,latitude: 30.670078,name: '万科·钻石广场',tagId: 1,categoryId: 1,address: '建设路10号'},
+        {id: 8,longitude: 104.104147,latitude: 30.669054,name: '高地中心',tagId: 1,categoryId: 1,address: '建设路9号'},
+        {id: 9,longitude: 104.075275,latitude: 30.601965,name: '成都高新技术产业开发区桂溪街道办事处',tagId: 1,categoryId: 1,address: '天仁路176号'},
+        {id: 10,longitude: 104.077467,latitude: 30.618422,name: '桂溪加油站(科华中路)',tagId: 1,categoryId: 1,address: '科华中路63号'},
+        {id: 12,longitude: 104.062087,latitude: 30.599805,name: '宜家家居(成都商场店)',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道宜家家居(成都商场店)'},
+        {id: 13,longitude: 104.06183,latitude: 30.601754,name: '欧尚(成都高新店)',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道欧尚(成都高新店)'},
+        {id: 14,longitude: 104.063986,latitude: 30.599547,name: '迪卡侬',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道都会路199号迪卡侬运动场'},
+        {id: 15,longitude: 104.060961,latitude: 30.601255,name: '必胜客',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道必胜客(成都高新店)'},
+        {id: 16,longitude: 104.060102,latitude: 30.601394,name: '宜家家居(成都商场店)',tagId: 1,categoryId: 1,address: '四川省成都市武侯区石羊场街道益州大道北段360号英祥·财富领域'},
+        {id: 17,longitude: 104.062903,latitude: 30.601855,name: '凯德广场',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道南洋小馆(凯德广场)凯德广场(新南店)'},
+        {id: 18,longitude: 104.062087,latitude: 30.599805,name: '宜家家居(成都商场店)',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道宜家家居(成都商场店)'},
+        {id: 19,longitude: 104.05815,latitude: 30.599565,name: '城市春天',tagId: 1,categoryId: 1,address: '四川省成都市武侯区石羊场街道成都云起茶宿客栈城市春天'},
+        {id: 20,longitude: 104.062087,latitude: 30.599805,name: '四川省城市车辆置业有限责任公司',tagId: 1,categoryId: 1,address: '四川省成都市武侯区石羊场街道火车南站西路865号四川省城市车辆置业有限责任公司'}
+      ],
+      markerList: [
+        {id: 1,longitude: 104.106946,latitude: 30.674249,name: '妈妈驿站',tagId: 1,categoryId:1},
+        {id: 11,longitude: 104.106946,latitude: 30.674253,name: '华贸广场',tagId: 1,categoryId:1},
+        {id: 2,longitude: 104.109191,latitude: 30.671637,name: '伊藤洋华堂(建设路店)',tagId: 1,categoryId: 1,address: '二环路东二段426号阳光新生活广场'},
+        {id: 3,longitude: 114.396345,latitude: 30.9455,name: '成都',tagId: 1,categoryId: 1},
+        {id: 4,longitude: 104.101876,latitude: 30.668877,name: '第五大道',tagId: 1,categoryId: 1,address: '建设路26号'},
+        {id: 5,longitude: 121.5273285,latitude: 31.21515044,name: '上海',tagId: 1,categoryId: 1},
+        {id: 6,longitude: 104.108444,latitude: 30.670595,name: '建设路东站(公交站)',tagId: 1,categoryId: 1,address: '6路;14路;42路;71路;72路;社区巴士1010路'},
+        {id: 7,longitude: 104.104973,latitude: 30.670078,name: '万科·钻石广场',tagId: 1,categoryId: 1,address: '建设路10号'},
+        {id: 8,longitude: 104.104147,latitude: 30.669054,name: '高地中心',tagId: 1,categoryId: 1,address: '建设路9号'},
+        {id: 9,longitude: 104.075275,latitude: 30.601965,name: '成都高新技术产业开发区桂溪街道办事处',tagId: 1,categoryId: 1,address: '天仁路176号'},
+        {id: 10,longitude: 104.077467,latitude: 30.618422,name: '桂溪加油站(科华中路)',tagId: 1,categoryId: 1,address: '科华中路63号'},
+        {id: 12,longitude: 104.062087,latitude: 30.599805,name: '宜家家居(成都商场店)',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道宜家家居(成都商场店)'},
+        {id: 13,longitude: 104.06183,latitude: 30.601754,name: '欧尚(成都高新店)',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道欧尚(成都高新店)'},
+        {id: 14,longitude: 104.063986,latitude: 30.599547,name: '迪卡侬',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道都会路199号迪卡侬运动场'},
+        {id: 15,longitude: 104.060961,latitude: 30.601255,name: '必胜客',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道必胜客(成都高新店)'},
+        {id: 16,longitude: 104.060102,latitude: 30.601394,name: '宜家家居(成都商场店)',tagId: 1,categoryId: 1,address: '四川省成都市武侯区石羊场街道益州大道北段360号英祥·财富领域'},
+        {id: 17,longitude: 104.062903,latitude: 30.601855,name: '凯德广场',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道南洋小馆(凯德广场)凯德广场(新南店)'},
+        {id: 18,longitude: 104.062087,latitude: 30.599805,name: '宜家家居(成都商场店)',tagId: 1,categoryId: 1,address: '四川省成都市武侯区桂溪街道宜家家居(成都商场店)'},
+        {id: 19,longitude: 104.05815,latitude: 30.599565,name: '城市春天',tagId: 1,categoryId: 1,address: '四川省成都市武侯区石羊场街道成都云起茶宿客栈城市春天'},
+        {id: 20,longitude: 104.062087,latitude: 30.599805,name: '四川省城市车辆置业有限责任公司',tagId: 1,categoryId: 1,address: '四川省成都市武侯区石羊场街道火车南站西路865号四川省城市车辆置业有限责任公司'}
       ]
     }
   },
@@ -410,7 +254,6 @@ export default {
       mapObj.setLimitBounds(bounds);
     },
     creatMap() {
-      var self = this;
       let mapObj = amapManager._map;
       var markers = [];
       this.markerList.forEach((item,index) => {
@@ -436,25 +279,23 @@ export default {
           zIndex: item.zIndex,
           riseOnHover: true,
           content:'<div>'+
-                    '<svg height="30px" width="30px"><use xlink:href="#chooseIcon'+category.iconId+'" fill="'+tag.color+'" stroke="'+tag.color+'" class="use-style"></use><span class="marker-title">'+item.title.substr(0,1)+'</span></svg>'+
+                    '<svg height="30px" width="30px"><use xlink:href="#chooseIcon'+category.iconId+'" fill="'+tag.color+'" stroke="'+tag.color+'" class="use-style"></use><span class="marker-title">'+item.name.substr(0,1)+'</span></svg>'+
                   '</div>'
         })
+        var self = this;
         marker.on('click', function() {
-          this.activeName = "0";
+          self.activeName = "0";
           self.window.visible = false;
           var markerId = this.getExtData();
           self.markerClustererShow = false;
           self.window.position = [item.longitude, item.latitude];
           self.currentMarkerId = markerId;
-          console.log(self.currentMarkerId)
-          this.markerList.forEach(item => {
+          self.markerList.forEach(item => {
             if(item.id == self.currentMarkerId){
               self.currentMarkerTagId = item.tagId;
               self.currentMarkerCategoryId = item.categoryId
             }
           })
-          // self.$refs.tagWindow.currentMarkerIdChange(self.currentMarkerId);
-          // self.$refs.categoryWindow.currentMarkerIdChange(self.currentMarkerId)
           self.$nextTick(() => {
             self.window.visible = true;
           });
