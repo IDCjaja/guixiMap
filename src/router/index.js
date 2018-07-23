dfbfb8c1119ba0eaa5bdd1aaa22ae4f6b1adde60
IDCjaja <838927992@qq.com>
2018-07-23 15:52:58 +0800

    Delete invalid path

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import admin from '@/pages/admin'
 import adminMobile from '@/pages/adminMobile'
-import select from '@/pages/select'
-import mobileInformation from '@/pages/mobileInformation'
-import search from '@/pages/search'
 
 Vue.use(Router)
 
@@ -19,21 +16,6 @@ export default new Router({
       path: '/mobile/admin',
       name: 'adminMobile',
       component: adminMobile,
-    },
-    {
-      path: '/mobile/select',
-      name: 'select',
-      component: select
-    },
-    {
-      path: '/mobile/mobileInformation',
-      name: 'mobileInformation',
-      component: mobileInformation
-    },
-    {
-      path: '/mobile/search',
-      name: 'search',
-      component: search
     }
   ]
 })
